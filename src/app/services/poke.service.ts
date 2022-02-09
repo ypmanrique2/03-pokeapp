247f8979fe0a93d25e1416c351ebd5ac4428f9d8
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-02-09 18:10:14 -0500

    03-pokeapp

---
+++ src/app/services/poke.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokeService {
+  private baseUrl: string = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) {}
+
+  getList(page: number = 0) {
+    return this.http.get(`${this.baseUrl}/pokemon/`, {
+      params: {
+        offset: page,
+        limit: 8,
+      },
+    });
+  }
+
+  getPokeData(name: string) {
+    return this.http.get(`${this.baseUrl}/pokemon/${name}`);
+  }
+}
